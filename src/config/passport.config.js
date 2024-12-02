c4c627f4dba0e10a2447ab511500aa2d88b8c576
Manuel <leonmanu@gmail.com>
2024-12-02 09:14:27 -0300

    4° - alguno arreglitos

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -17,7 +17,10 @@ passport.use(
         {
             clientID: "158320560302-uijgnhspve6ntibrgkcn5gg9juuuvvmk.apps.googleusercontent.com",
             clientSecret: "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",
-            callbackURL: "http://localhost:5000/oauth2callback",
+            callbackURL: process.env.NODE_ENV === 'production'
+            ? "https://articulacion.onrender.com//auth/google/callback"
+            : "http://localhost:3000/auth/google/callback",
+            passReqToCallback : true
         },
         async (accessToken, refreshToken, profile, done) => {
             console.log('Perfil de usuario:', profile);
